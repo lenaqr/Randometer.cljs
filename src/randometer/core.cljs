9c166cbba32694b5768fedab2e06a07e963b3857
Anthony Lu <lua@mit.edu>
2015-08-20 15:11:22 -0400

    Implement the "choose one" game

--- src/randometer/core.cljs
+++ src/randometer/core.cljs
@@ -36,14 +36,63 @@
 (defn shuffle-boards [boards]
   (zipmap (range) (shuffle (range (count boards)))))
 
-;; define your app data so that it doesn't get over-written on reload
+(defn board-element [board]
+  [:div.board {:style {:display "inline-block"
+                       :vertical-align "middle"
+                       :margin "0.5em"}}
+   (for [row board]
+     [:div.row (apply str (map show-trial row))])])
 
-(defonce app-state (atom {:text "Hello world!"}))
+(defn board-chooser [{:keys [on-guess on-reset flavortext answertext]} boards keymap guess]
+  [:fieldset {:on-click #(when (some? guess) (on-reset))}
+   [:legend flavortext]
+   (for [ix (range (count boards))]
+     ^{:key ix}
+     [:div {:style {:background-color (when (some? guess)
+                                        (cond
+                                         (= 0 (keymap ix)) "#efe"
+                                         (= guess ix) "#fee"))}}
+      [:label
+       [:input {:type :radio
+                :name "guess"
+                :checked (= ix guess)
+                :disabled (some? guess)
+                :on-click #(on-guess ix)
+                :style {:display "inline-block"
+                        :vertical-align "middle"}}]
+       [board-element (nth boards (keymap ix))]
+       [:span {:style {:display (when (nil? guess) "none")}}
+        (nth answertext (keymap ix))]]])
+   (when (some? guess)
+     [:button {:on-click #(on-reset) :style {:margin-top "0.5em"}}
+      "play again"])])
 
-(defn hello-world []
-  [:h1 (:text @app-state)])
+(defn choose-one [x xs]
+  (let [game-state (atom {})
+        init-game! (fn []
+                     (let [boards (map #(puzzle muted-generator %) (cons x xs))
+                           keymap (shuffle-boards boards)]
+                      (swap! game-state assoc
+                             :boards boards
+                             :keymap keymap
+                             :guess nil)))
+        guess! (fn [ix]
+                 (swap! game-state assoc :guess ix))]
+    (init-game!)
+    (fn []
+      (let [{:keys [boards keymap guess]} @game-state]
+        [board-chooser {:on-guess #(guess! %)
+                        :on-reset #(init-game!)
+                        :flavortext "Which is truly random?"
+                        :answertext (map #(str % " switch") (cons x xs))}
+         boards keymap guess]))))
 
-(reagent/render-component [hello-world]
+(defn main-page []
+  [:div
+   [:h1 "Randometer"]
+   [choose-one 0.5 [0.25 0.33 0.67 0.75]]])
+
+(reagent/render-component [main-page]
                           (. js/document (getElementById "app")))
 
 
